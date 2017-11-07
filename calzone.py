8856b09d60059498c510557fd6e2842debf34873
200020088 <neha@ge.com>
2017-11-07 18:54:27 -0500

    deleting files

--- calzone.py
+++
@@ -1,6 +0,0 @@
-from pizza_dish import PizzaDish
-
-class Calzone(PizzaDish):
-    def __init__(self):
-        self.setName("calzone")
-        self.setSize("small")
--- pizza.py
+++
@@ -1,6 +0,0 @@
-from pizza_dish import PizzaDish
-
-class Pizza(PizzaDish):
-    def __init__(self):
-        self.setName("pizza")
-        self.setSize("large")
